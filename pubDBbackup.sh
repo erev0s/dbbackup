cab306050acf66af7f91756aca115b35bd7f79e7
erev0s <vaslion13@yahoo.gr>
2016-11-16 14:08:59 +0200

    improoved
    replaced echo with printf for better results in diferrent versions
    added a check that mysqldump finished properly before deleting databases
    it also notifies you about deleting now

--- pubDBbackup.sh
+++ pubDBbackup.sh
@@ -21,29 +21,31 @@ fi
 DBS="$(mysql -u$USER -p$PASSWORD -Bse 'show databases')"
 echo "$DBS"
 
+errorcounter=0 #innitializing a variable to count errors in mysqldump
 
 #if your db starts with an _ then ignore it this way you can create dbs with _ if you want them ignored
 #need to add a list of databases so you can add a database you need to be ignored
 for db in $DBS; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]] && [[ "$db" != "performance_schema" ]] ; then
-        echo "Dumping database: $db"
+        printf "Dumping database: $db\n"
 		        mysqldump --force --opt --user=$USER --password=$PASSWORD --databases $db > $path/`date +%Y%m%d`.$db.sql
 				exitcode=$?
         gzip $path/`date +%Y%m%d`.$db.sql
 		FILESIZE=$(stat -c%s "$path/`date +%Y%m%d`.$db.sql.gz")
-		printf "$db Backed up with EXITCODE [ $exitcode ] and with size $FILESIZE bytes\n";	
-	#create an if to notify us in case of bad mysqldump
-	if [ $exitcode -ne 0 ]; then
-	curl -s \
-	-F "token=$TOKEN" \
+		printf "$db Backed up with EXITCODE [ $exitcode ] and with size $FILESIZE bytes\n";
+		
+		#create an if to notify us in case of bad mysqldump
+		if [ $exitcode -ne 0 ]; then
+		curl -s \
+		-F "token=$TOKEN" \
         -F "user=$USERID" \
         -F "message=$db ERROR BACKING UP === errorcode = $exitcode   " \
         -F "priority=1"\
         https://api.pushover.net/1/messages.json	
-		echo "$db ERROR BACKING UP === errorcode = $exitcode" >> $STATUS
-		
+		printf "\n\n$db ERROR BACKING UP === errorcode = $exitcode \n\n" >> $STATUS
+		((errorcounter++))
 		else
-		echo "$db Backed up with EXITCODE [ $exitcode ] and with size $FILESIZE bytes" >> $STATUS
+		printf "\n$db Backed up with EXITCODE [ $exitcode ] and with size $FILESIZE bytes\n" >> $STATUS
 		fi
 		
 	fi
@@ -51,11 +53,12 @@ done
 
 
 #method to remove old database backups
-find $DELPATH* -mtime +5 -exec rm {} \;
-
-
+if [ $errorcounter -ne 0 ]; then
+	printf "Not deleting anything due to errors in mysqldump\n" >> $STATUS
 else
+	find $DELPATH* -mtime +5 -exec rm {} \;
 	printf "\nFiles older than 5 days have been deleted\n" >> $STATUS
+fi
 
 #notify me of all the results
         curl -s \
@@ -66,8 +69,4 @@ else
         https://api.pushover.net/1/messages.json
 		
 #by mail too
-echo "$(cat $STATUS)" | mail -s "Database Backup Results" $MAIL	
-
-
-
-
+printf "Server hostname is $(hostname)\n$(cat $STATUS)" | mail -s "Database Backup Results" $MAIL
